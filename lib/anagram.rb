3334a173c67c781abd19f675a5abe6cb494ed497
shuaib shaban <shuaib>
2023-02-10 09:15:58 +0300

    passed test

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word.downcase
+    end
+  
+    def match(words)
+      words.select { |w| w.downcase.chars.sort == @word.chars.sort && w.downcase != @word }
+    end
+  end
+  
